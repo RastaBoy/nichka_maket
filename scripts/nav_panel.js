2abb9f743cb3b30be5ca93d54be7dc880d19f8a1
RastaBoy <trustyone3224@gmail.com>
2021-07-12 18:50:33 +0600

    first page and navpanel

---
+++ scripts/nav_panel.js
@@ -0,0 +1,10 @@
+function navController() {
+    let nav = document.getElementById("navpanel");
+
+    if(nav.style.display === "") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "";
+    }
+
+}
